a1013cbd4665490333ca4e958269eb2e095cadb6
salimm <salimm@cs.pitt.edu>
2018-04-22 15:38:18 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ module1 = Extension('plmsgpackcapi',
 setup(
   name = 'pylods-msgpack',
   packages = ['pylodsmsgpack'], # this must be the same as the name above
-  version = '0.3.2',
+  version = '0.3.3',
   description = 'Msgpack extension for pylods package',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/salimm/pylods-msgpack', # use the URL to the github repo
-  download_url = 'http://github.com/salimm/pylods-msgpack/archive/0.3.2.tar.gz', # I'll explain this in a second
+  download_url = 'http://github.com/salimm/pylods-msgpack/archive/0.3.3.tar.gz', # I'll explain this in a second
   keywords = ['python','serialization','deserialization',
               'parser','json','object oriented','fast','extendable',
               'type based','jackson json', 'msgpack', 'umsgpack'], # arbitrary keywords
