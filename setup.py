523ca7bd35690d1cb9ae0f256c1078959b5d6b1d
salimm <salimm@cs.pitt.edu>
2018-04-22 14:14:19 -0400

    updated setup.py and added MANIFEST.in

--- setup.py
+++ setup.py
@@ -13,14 +13,16 @@ module1 = Extension('plmsgpackcapi',
 setup(
   name = 'pylods-msgpack',
   packages = ['pylodsmsgpack'], # this must be the same as the name above
-  version = '0.3.0',
+  version = '0.3.1',
   description = 'Msgpack extension for pylods package',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/salimm/pylods-msgpack', # use the URL to the github repo
-  download_url = 'http://github.com/salimm/pylods-msgpack/archive/0.3.0.tar.gz', # I'll explain this in a second
-  keywords = ['python','serialization','deserialization','paser','json','object oriented','fast','extendable','type based','jackson json', 'msgpack', 'umsgpack'], # arbitrary keywords
+  download_url = 'http://github.com/salimm/pylods-msgpack/archive/0.3.1.tar.gz', # I'll explain this in a second
+  keywords = ['python','serialization','deserialization',
+              'parser','json','object oriented','fast','extendable',
+              'type based','jackson json', 'msgpack', 'umsgpack'], # arbitrary keywords
   classifiers = ['Programming Language :: Python'],
   install_requires=['pylods','msgpackstream', 'umsgpack'],
   ext_modules = [module1]
